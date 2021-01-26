a07c5e76c7d4112ec5ff5443cbcc5d653b011a98
ETHIHAS27 <71704339+ETHIHAS27@users.noreply.github.com>
2021-01-26 16:23:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+    })
+
+    camera = document.getElementById("camera");
+
+    Webcam.attach('#camera');
+
+    function takesnapshot(){
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML = "<img id='img_snap' src="+data_uri+">";
+        })
+    }
+
+    function check(){
+        img = document.getElementById("img_snap");
+
+        classifier.classify(img,gotresult);
+    }
+
+    console.log("ml5 version",ml5.version);
+
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ACRF8lGdR/model.json",modelLoded);
+
+function modelLoded(){
+    console.log("model loded!");
+}
+
+function gotresult(error,results){
+    if(error){
+        console.log("error!");
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed("3");
+    }
+}
+
